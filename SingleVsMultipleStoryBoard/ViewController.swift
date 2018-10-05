3a05c13c094517d788a3cace0e4a7b4fc69acaf9
Ankit Kumar <ankitdroiddeveloper@gmail.com>
2018-10-05 17:04:02 +0800

    Initial Commit

---
+++ SingleVsMultipleStoryBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingleVsMultipleStoryBoard
+//
+//  Created by Ankit Kumar on 05/10/2018.
+//  Copyright © 2018 Capital Platforms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
